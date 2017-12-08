cc3865714d8ab49193f3fa2fe63041810f18b840
Jason Newman <jason@squarepenguin.com>
2017-12-08 11:54:35 -0600

    add tokenize package

---
+++ internal/tokenize/tokenize.go
@@ -0,0 +1,55 @@
+package tokenize
+
+import (
+	"bufio"
+	"io"
+	"strings"
+)
+
+// Tokenize takes a reader and returns a string slice
+// Where each element in the string slice is a block of text
+// not separated by more than one new line.
+func Tokenize(r io.Reader) ([]string, error) {
+
+	b := bufio.NewReader(r)
+
+	tokens := []string{}
+	current := ""
+	count := 0
+
+	for {
+		l, err := b.ReadString('\n')
+		if err == io.EOF {
+			if len(current) > 0 {
+				tokens = append(tokens, current)
+			}
+			break
+
+		} else if err != nil {
+			return nil, err
+		}
+
+		l = strings.TrimSpace(l)
+		if len(l) == 0 {
+			if len(current) == 0 {
+				continue
+			}
+
+			count = 0
+			tokens = append(tokens, current)
+			current = ""
+			continue
+		}
+
+		sp := " "
+		if count == 0 {
+			sp = ""
+		}
+		current = current + sp + string(l)
+		count++
+
+	}
+
+	return tokens, nil
+
+}
---
+++ internal/tokenize/tokenize_test.go
@@ -0,0 +1,37 @@
+package tokenize
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+func TestTokenize(t *testing.T) {
+
+	r := bytes.NewBuffer([]byte(sampleText))
+
+	tokens, err := Tokenize(r)
+	if err != nil {
+		t.Fatalf("unable to split sample text: %s\n", err)
+	}
+
+	if len(tokens) != 3 {
+		fmt.Printf("%#v\n", tokens)
+		t.Fatalf("unexpected number of tokens [%d]\n", len(tokens))
+	}
+
+	fmt.Printf("%#v\n", tokens)
+
+}
+
+const sampleText = `
+This is my sentence
+broken onto two lines.
+
+
+
+This the the second 
+sentence broken onto two lines.
+
+Final sentence.
+`
